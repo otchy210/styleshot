a4c165005081659d529e59c1b5215a1bba81d457
Otchy <otchy@MBP.local>
2017-07-24 00:35:18 +0900

    [CE] Add minify option

--- chrome-extension/scripts/build.js
+++ chrome-extension/scripts/build.js
@@ -1,10 +1,22 @@
 'use strict';
 
+const command = require('commander');
+command
+    .option('-m, --minify', 'Minify files')
+    .parse(process.argv);
 const webpack = require('webpack');
-const config = require('../webpack.config.js');
+let config = require('../webpack.config.js')
+if (command.minify) {
+    config.plugins = [new webpack.optimize.UglifyJsPlugin()];
+    let filename = config.output.filename;
+    const len = filename.length;
+    filename = filename.substr(0, len-3) + '.min' + filename.substr(len-3);
+    config.output.filename = filename
+}
 const compiler = webpack(config);
 compiler.run((err, stats) => {
     if (err) {
         console.log(err, stats);
     }
 });
+
